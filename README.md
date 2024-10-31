1481c2a90a6006491cca14beebae5e880083d16d
haile01 <ledinhhai1312@gmail.com>
2024-11-01 00:11:11 +0700

    poop-ization

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 ## Github starred list
 Some low-effort effort to automate categorizing starred repos on Github cuz RestAPI ain't do that anytime soon
+
+### How to?
+Initialize with github username and cookie header grabbed from `https://github.com/<urname>?tab=stars`
+*Yep that would only last for 2 weeks sorry*
+
+Use 4 methods like in the example:
+- `create_list(list_name, list_desc)`
+- `add_repo(repo_name, list_name)`
+- `remove_repo(repo_name, list_name)`
+- `delete_list(repo_name)`
+
+### But... Python Object Oriented Programming?
+Yup I hate that but I'm lazy, and it's intended to be imported elsewhere.
--- main.py
+++ main.py
@@ -1,69 +1,62 @@
 import requests
 import re
 
-HOST = "https://github.com"
-USER = "haile01"
-CSRF_TOKEN_PATTERN = r'<input type="hidden" name="authenticity_token" value="(.+?)" autocomplete="off" />'
-REPO_ID_PATTERN = r'<input type="hidden" name="repository_id" value="([0-9]+)">'
-COOKIE = """..."""
+class ListHelper:
+  def __init__(self, user, cookie):
+    self.HOST = "https://github.com"
+    self.CSRF_TOKEN_PATTERN = r'<input type="hidden" name="authenticity_token" value="(.+?)" autocomplete="off" />'
+    self.REPO_ID_PATTERN = r'<input type="hidden" name="repository_id" value="([0-9]+)">'
 
-def parse_cookie(s):
-  cookies = {}
-  for c in s.split(';'):
-    name, value = c.strip().split('=')
-    cookies[name] = value
+    self.user = user
+    self.__get, self.__post = self.__init_requests(cookie)
 
-  return cookies
+  def __parse_cookie(self, s):
+    cookies = {}
+    for c in s.split(';'):
+      name, value = c.strip().split('=')
+      cookies[name] = value
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.6668.71 Safari/537.36",
-    "Accept": "text/html, application/json",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Content-Type": "application/x-www-form-urlencoded",
-    "X-Requested-With": "XMLHttpRequest",
-    "Origin": "https://github.com"
-}
+    return cookies
 
-cookies = parse_cookie(COOKIE)
+  def __init_requests(self, cookie):
+    headers = {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.6668.71 Safari/537.36",
+      "Accept": "text/html, application/json",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "X-Requested-With": "XMLHttpRequest",
+      "Origin": "https://github.com"
+    }
 
-def request(method, *args, **kwargs):
-  print(args, kwargs)
-  if method == 'get':
-    return requests.get(*args, **kwargs, cookies=cookies)
-  else:
-    return requests.post(*args, **kwargs, headers=headers, cookies=cookies)
+    cookies = self.__parse_cookie(cookie)
 
-def search_before_text(s, pattern, text):
-  ind = s.find(text)
-  if ind != -1:
-    s = s[:ind]
+    def get(path, *args, **kwargs):
+      print('get', args, kwargs)
+      return requests.get(self.HOST + path, *args, **kwargs, cookies=cookies)
 
-  found = re.findall(pattern, s)
-  if len(found) == 0:
-    return None
+    def post(path, *args, **kwargs):
+      print('post', args, kwargs)
+      return requests.post(self.HOST + path, *args, **kwargs, headers=headers, cookies=cookies)
 
-  return found[-1]
+    return get, post
 
-def create_list(name, desc):
-  r = request('get', f'{HOST}/{USER}?tab=stars')
-  token = search_before_text(r.text, CSRF_TOKEN_PATTERN, "Create a list to organize your starred repositories.")
-  assert token is not None, "Can get token"
+  def __search_before_text(self, s, pattern, text):
+    ind = s.find(text)
+    if ind != -1:
+      s = s[:ind]
 
-  data = {
-      "authenticity_token": token,
-      "user_list[name]": name,
-      "user_list[description]": desc
-  }
+    found = re.findall(pattern, s)
+    if len(found) == 0:
+      return None
 
-  r = request('post', f'{HOST}/stars/{USER}/lists', data=data)
-  assert r.status_code == 200, "Should be OK"
+    return found[-1]
 
-def get_list_mapping_and_token(repo):
-  mapping = {}
-  r = request('get', f'{HOST}/{repo}/lists')
+  def __get_list_mapping_and_token(self, repo):
+    mapping = {}
+    r = self.__get(f'/{repo}/lists')
 
-  pattern = r"""<input
+    pattern = r"""<input
                     type="checkbox"
                     class="mx-0 js-user-list-menu-item"
                     name="list_ids\[\]"
@@ -73,54 +66,79 @@ def get_list_mapping_and_token(repo):
                   <span data-view-component="true" class="Truncate ml-2 text-normal f5">
     <span data-view-component="true" class="Truncate-text">(.+?)</span>"""
 
-  found = re.findall(pattern, r.text, re.MULTILINE)
-  for l in found:
-    mapping[l[1]] = l[0]
+    found = re.findall(pattern, r.text, re.MULTILINE)
+    for l in found:
+      mapping[l[1]] = l[0]
 
-  # token
-  found = re.findall(CSRF_TOKEN_PATTERN, r.text)
+    # token
+    found = re.findall(self.CSRF_TOKEN_PATTERN, r.text)
 
-  return mapping, found[0]
+    return mapping, found[0]
 
-def get_repo_id(repo):
-  r = request('get', f'{HOST}/{repo}/lists')
-  found = re.findall(REPO_ID_PATTERN, r.text)
-  assert len(found) > 0, "Repo doesn't exist"
+  def __get_repo_id(self, repo):
+    r = self.__get(f'/{repo}/lists')
+    found = re.findall(self.REPO_ID_PATTERN, r.text)
+    assert len(found) > 0, "Repo doesn't exist"
 
-  return found[0]
+    return found[0]
 
-def repo_to_list(repo, _list, add=True):
-  mapping, token = get_list_mapping_and_token(repo)
-  assert _list in mapping, "List doesn't exist"
+  def __repo_to_list(self, repo, _list, add=True):
+    mapping, token = self.__get_list_mapping_and_token(repo)
+    assert _list in mapping, "List doesn't exist"
 
-  list_id = mapping[_list]
+    list_id = mapping[_list]
 
-  data = {
+    data = {
       "_method": "put",
       "authenticity_token": token,
-      "repository_id": get_repo_id(repo),
+      "repository_id": self.__get_repo_id(repo),
       "context": "user_list_menu",
       "list_ids[]": list_id if add else ""
-  }
+    }
 
-  r = request('post', f'{HOST}/{repo}/lists', data=data)
-  assert r.status_code == 200, "Should be OK"
+    r = self.__post(f'/{repo}/lists', data=data)
+    assert r.status_code == 200, "Should be OK"
 
-def add_repo(repo, _list):
-  repo_to_list(repo, _list)
+  def create_list(self, name, desc):
+    r = self.__get(f'/{self.user}?tab=stars')
+    token = self.__search_before_text(r.text, self.CSRF_TOKEN_PATTERN, "Create a list to organize your starred repositories.")
+    assert token is not None, "Can get token"
 
-def remove_repo(repo, _list):
-  repo_to_list(repo, _list, add=False)
+    data = {
+      "authenticity_token": token,
+      "user_list[name]": name,
+      "user_list[description]": desc
+    }
 
-def delete_list(_list):
-  r = request('get', f'{HOST}/stars/{USER}/lists/{_list}')
-  token = search_before_text(r.text, CSRF_TOKEN_PATTERN, '<button type="submit" data-view-component="true" class="btn-danger btn">')
-  assert token is not None, "Can get token"
+    r = self.__post(f'/stars/{self.user}/lists', data=data)
+    assert r.status_code == 200, "Should be OK"
 
-  data = {
+  def add_repo(self, repo, _list):
+    self.__repo_to_list(repo, _list)
+
+  def remove_repo(self, repo, _list):
+    self.__repo_to_list(repo, _list, add=False)
+
+  def delete_list(self, _list):
+    r = self.__get(f'/stars/{self.user}/lists/{_list}')
+    token = self.__search_before_text(r.text, self.CSRF_TOKEN_PATTERN, '<button type="submit" data-view-component="true" class="btn-danger btn">')
+    assert token is not None, "Can get token"
+
+    data = {
       "_method": "delete",
       "authenticity_token": token
-  }
+    }
 
-  r = request('post', f'{HOST}/stars/{USER}/lists/{_list}', data=data)
-  assert r.status_code == 200, "Should be OK"
+    r = self.__post(f'/stars/{self.user}/lists/{_list}', data=data)
+    assert r.status_code == 200, "Should be OK"
+
+if __name__ == "__main__":
+  cookie = """..."""
+  helper = ListHelper("haile01", cookie)
+  helper.create_list("test1234", "ahgihihiihihi")
+  input()
+  helper.add_repo("codeigniter4/shield", "test1234")
+  input()
+  helper.remove_repo("codeigniter4/shield", "test1234")
+  input()
+  helper.delete_list("test1234")
